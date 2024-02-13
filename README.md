b8733323d31064666f017e751ab861dd0c660cb0
Josh Robbs <josh@joshrobbs.com>
2024-02-13 14:58:00 -0500

    Initial files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Social Icons
+
+**Note** This system requires ACF Pro
+
+Status: **_In development_**
+
+## Contact
+
+Have a question or comment, hit me up at https://twitter.com/_JoshRobbs
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Simple Social Icons shortcode
+ *
+ * @author Josh Robbs <josh@joshrobbs.com>
+ * @since 2024-02-13
+ * @package Simple_Social_Icons
+ */
+
+namespace SimpleSocialIcons;
+
+defined( 'ABSPATH' ) || die();
+
+define( 'JWR_SSI_PATH', plugin_dir_path( __FILE__ ) );
+define( 'JWR_SSI_SPRITE_URL', plugin_dir_url( __FILE__ ) . 'images/sprite.png' );
+
+/*
+	[] update control panel -> add repeater
+	[] add control panel -> URLs for social media
+	[] create output (sc?)
+	[] create repo
+*/
